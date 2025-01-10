6a68729502293a620b6d987819e2ffd8d360b59b
Malek Mansour <malek24mansour@gmail.com>
2025-01-10 08:27:34 -0700

    Update preload.js

--- system-overlay/preload.js
+++ system-overlay/preload.js
@@ -9,9 +9,7 @@ contextBridge.exposeInMainWorld('systemInfo', {
             gpu: await si.graphics(),
             memory: await si.mem(),
             battery: await si.battery(),
-            network: await si.networkStats(),
-            volume: 50, // Placeholder for volume (needs additional APIs)
-            vpn: false // Placeholder for VPN status
+            volume: 50, 
         };
     }
 });
